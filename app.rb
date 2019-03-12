0a999401af0da0675ff40abce4220b2e3c263f2d
github username <github email address>
2019-03-12 09:01:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+# Create a route that responds to a GET request at /newteam
+  get '/newteam' do
+    erb :newteam
+  end
+  # render it in the GET /newteam route??
+
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+
+    erb :team
+    # on submission, pass the data to the team.erb to render the view
+  end
 
 end
